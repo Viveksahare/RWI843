87a4efbf9a1674c75e395235f6428df49c0f744b
Vivek Kewalram Sahare <114302698+Viveksahare@users.noreply.github.com>
2024-05-23 16:44:32 +0530

    Check whether the static block print or not.
    Day-3

---
+++ Check_StaticBlock.java
@@ -0,0 +1,10 @@
+public class Check_StaticBlock {
+    // Static block
+    static {
+        System.out.println("Hello from the static block!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World from the main block!");
+    }
+}
