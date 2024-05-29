11e8aad86f81ed845d87f477f9c288d82e7ad20c
Vivek Kewalram Sahare <114302698+Viveksahare@users.noreply.github.com>
2024-05-29 14:24:33 +0530

    Polymorphism - Method Overloading
    Day-8

---
+++ Method_Overloading.java
@@ -0,0 +1,8 @@
+public class Method_Overloading {
+    static int Mul(int num_1, int num_2){
+        return num_1 * num_2;
+    }
+    static double Mul(double num_1, double num_2){
+        return num_1 * num_2;
+    }
+}
--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        Student st = new Student();
-        st.setName("Vivek");
-        System.out.println(st.getName());
+        System.out.println(Method_Overloading.Mul(41,56));
+        System.out.println(Method_Overloading.Mul(45.2,652.0));
     }
 }
