aa9fee34800cd17932955345b68005d37d820559
Vivek Kewalram Sahare <114302698+Viveksahare@users.noreply.github.com>
2024-05-29 14:19:50 +0530

    Encapsulation
    Day-7

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        Bike bike = new Honda();
-            bike.run();
-            bike.GearChange();
+        Student st = new Student();
+        st.setName("Vivek");
+        System.out.println(st.getName());
     }
 }
