8cd4fb4c7ab8d9c79c52fa51cc3a67f11062a6c0
Vivek Kewalram Sahare <114302698+Viveksahare@users.noreply.github.com>
2024-05-29 14:22:28 +0530

    Inheritance - Check which class is parent class
    Day-7

---
+++ A.java
@@ -0,0 +1,9 @@
+public class A extends B{
+    float bonus = 10000;
+
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.salary);
+        System.out.println(a.bonus);
+    }
+}
---
+++ B.java
@@ -0,0 +1,3 @@
+public class B {
+    float salary = 20000f;
+}
