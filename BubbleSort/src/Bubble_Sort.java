64b57e106c3b5f6dc259eaff93238036600229e4
Vivek Kewalram Sahare <114302698+Viveksahare@users.noreply.github.com>
2024-06-06 14:54:48 +0530

    Add files via upload

---
+++ BubbleSort/src/Bubble_Sort.java
@@ -0,0 +1,8 @@
+public class Bubble_Sort {
+    public static void PrintArray(int arr[]){
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
